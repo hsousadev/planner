b911ae501850f9ded753bd1ff1a57e1eb48212c6
hsousadev <henriquesousa.dev@gmail.com>
2024-10-01 20:13:44 -0300

    feat: creating and listing actitivies

--- src/pages/trip-details/create-activity-modal.tsx
+++ src/pages/trip-details/create-activity-modal.tsx
@@ -1,5 +1,10 @@
+import { FormEvent } from "react";
+import { useParams } from "react-router-dom";
+
 import { Calendar, Tag, X } from "lucide-react";
 import { Button } from "../../components/button";
+
+import { api } from "../../lib/axios";
 
 interface CreateActivityModalProps {
   closeCreateActivityModal: () => void;
@@ -8,6 +13,30 @@ interface CreateActivityModalProps {
 export function CreateActivityModal({
   closeCreateActivityModal,
 }: CreateActivityModalProps) {
+  const { tripId } = useParams();
+
+  async function createActivity(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const data = new FormData(event.currentTarget);
+
+    const title = data.get("title")?.toString();
+    const occurs_at = data.get("occurs_at")?.toString();
+
+    if (!title || !occurs_at) {
+      return;
+    }
+
+    await api.post(`/trips/${tripId}/activities`, {
+      title,
+      occurs_at,
+    });
+
+    closeCreateActivityModal();
+
+    window.location.reload();
+  }
+
   return (
     <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
       <div className="w-[640px] rounded-xl py-5 px-6  bg-zinc-100 space-y-5">
@@ -23,11 +52,11 @@ export function CreateActivityModal({
           </p>
         </div>
 
-        <form className="space-y-3">
+        <form onSubmit={createActivity} className="space-y-3">
           <div className="h-14 px-4 bg-zinc-200 border border-zinc-300 rounded-lg flex items-center gap-2">
             <Tag className="text-zinc-400 size-5" />
             <input
-              name="name"
+              name="title"
               placeholder="Qual a atividade"
               className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
             />
